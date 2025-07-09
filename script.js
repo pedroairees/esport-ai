3f042e8bc2b1aa73d38b2017335a27a29b488aff
pedroairees <pedrocoelho2209@gmail.com>
2025-07-09 18:37:24 -0300

    logical part

--- script.js
+++ script.js
@@ -6,9 +6,36 @@ const askButton = document.getElementById('askButton')
 
 const aiResponse = document.getElementById('aiResponse')
 
+const perguntarAI = async (question, game, apiKey) => {
+
+}
+
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    console.log(event)
+    const apiKey = apiKeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    console.log({apiKey, game, question})
+
+    if(apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
+
+    askButton.disabled = true
+    askButton.textContent = 'Perguntando...'
+    askButton.classList.add('loading')
+
+    try {
+        perguntarAI(question, game, apiKey)
+    }catch(error) {
+        console.log('Erro: ', error)
+    }finally {
+        askButton.disabled = false
+        askButton.textContent = 'Perguntar'
+        askButton.classList.remove('loading')
+    }
 })
 
-//Estudar sobre isso com a IA antes de continuar
+//Estudar sobre isso com a IA 
